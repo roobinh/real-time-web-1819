068c13e6f4cb7ee8030c6195d498feebce3b17a3
Robin Hofman <roobin1999@gmail.com>
2019-04-09 13:12:47 +0200

    basic chat app working

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var createError = require('http-errors');
+
 var express = require('express');
 var path = require('path');
 
-var indexRouter = require('./routes/index');
-var pathRouter = require('./routes/path');
-
 var app = express();
+var socket = require('socket.io')
+
 var port = process.env.PORT || 2100
+var server = app.listen(port, () => console.log(`App running, listening on port ${port}!`))
+
+var io = socket(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,9 +18,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routers
+var indexRouter = require('./routes/index');
+var pathRouter = require('./routes/path');
+
 app.use('/', indexRouter);
 app.use('/path', pathRouter);
 
-module.exports = app;
+// Socket.io
+io.on('connection', function(socket) {
+    console.log("made socket connection with client...", socket.id)
 
-app.listen(port, () => console.log(`App running, listening on port ${port}!`))
+    socket.emit('welcome', {welcome: 'to my socket'});
+
+    //when chat is recieved, send message to all sockets
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    })
+})
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,31 @@
 console.log("script.js imported.")
+
+// Socket.io
+console.log('Making connection...')
+var socket = io.connect('http://localhost:2100');
+console.log('Connected.')
+
+// Variables
+var chat = document.getElementById('chat');
+var chatwindow = document.getElementById('chat-window');
+var output = document.getElementById('output');
+var handle = document.getElementById('handle');
+var message = document.getElementById('message');
+var send = document.getElementById('send');
+
+send.addEventListener("click", function(){
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+})
+
+socket.on('chat', function(data) {
+    console.log("message recieved.");
+    console.log(data)
+    output.innerHTML += "<br>" + data.handle + ": " + data.message;
+})
+
+socket.on('welcome', function(data) {
+    console.log(data);
+})
